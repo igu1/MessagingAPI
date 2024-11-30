ebb8bd8e4a99d76eebcaec9da7ac2982dd960798
Eesa Zakariyya <eesaard@gmail.com>
2024-11-30 08:21:42 +0530

    vupdate

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MessagingAPI',
-    version='0.1',
+    name='messaging-api-wrapper',
+    version='0.1.0',
+    description='A Python package for sending WhatsApp and Email messages.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Eza',
+    author_email='eesaard@gmail.com',
+    url='https://github.com/igu1/MessagingAPI',
     packages=find_packages(),
     install_requires=[
         'requests',
         "Jinja2"
     ],
-    description='A package for sending emails and WhatsApp messages',
-    author='Eza',
-    author_email='eesaard@gmail.com',
-    url='https://github.com/igu1/MessagingAPI',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
